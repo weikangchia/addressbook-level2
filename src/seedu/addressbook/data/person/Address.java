f5a89825c4da11678f5621d927631e5546ab8d89
Wei Kang <weikangchia@gmail.com>
2016-09-03 23:45:49 +0800

    Make Address class implements Printable

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,11 +6,12 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address implements Printable {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_PREFIX = " Address: ";
 
     public final String value;
     private boolean isPrivate;
@@ -54,5 +55,10 @@ public class Address {
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+
+    @Override
+    public String getPrintableString() {
+        return isPrivate ? "" : ADDRESS_PREFIX + value;
     }
 }
