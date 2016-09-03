5a6d2f6a7457b327d7a37e221c780905dc1307e9
Wei Kang <weikangchia@gmail.com>
2016-09-03 23:46:33 +0800

    Make Phone class implements Printable

--- src/seedu/addressbook/data/person/Phone.java
+++ src/seedu/addressbook/data/person/Phone.java
@@ -6,11 +6,12 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's phone number in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidPhone(String)}
  */
-public class Phone {
+public class Phone implements Printable {
 
     public static final String EXAMPLE = "123456789";
     public static final String MESSAGE_PHONE_CONSTRAINTS = "Person phone numbers should only contain numbers";
     public static final String PHONE_VALIDATION_REGEX = "\\d+";
+    public static final String PHONE_PREFIX = " Phone: ";
 
     public final String value;
     private boolean isPrivate;
@@ -55,5 +56,10 @@ public class Phone {
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+
+    @Override
+    public String getPrintableString() {
+        return isPrivate ? "" : PHONE_PREFIX + value;
     }
 }
