fb95d5f9c67ff5c05c8e8292599e494f0b949c88
Wei Kang <weikangchia@gmail.com>
2016-09-04 17:23:19 +0800

    Add isSimilar method in Name class

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -61,4 +61,12 @@ public class Name {
         return fullName.hashCode();
     }
 
+    /**
+     * Returns true of the other name is very similar to this name.
+     * Two names are considered similar if ...
+     */
+    public Object isSimilar(Name other) {
+        return true;
+    }
+
 }
--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -1,10 +1,14 @@
 package seedu.addressbook.data.person;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
 
 public class NameTest {
     @Test 
-    public void testIsSimilar() {
+    public void testIsSimilar() throws IllegalValueException {
         Name name1 = new Name("John");
         Name name2 = new Name("John");
         
