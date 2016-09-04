77994c6eb724aacec5f9eaec5b70ba730066d9b0
Wei Kang <weikangchia@gmail.com>
2016-09-04 20:00:28 +0800

    Refractor code

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -7,57 +7,83 @@ import org.junit.Test;
 import seedu.addressbook.data.exception.IllegalValueException;
 
 public class NameTest {
+
     @Test
-    public void testIsSimilar() throws IllegalValueException {
+    public void isSimilar_exactName_returnsTrue() throws IllegalValueException {
         Name name1 = new Name("John");
         Name name2 = new Name("John");
 
         assertEquals(true, name1.isSimilar(name2));
+    }
+
+    @Test
+    public void isSimilar_otherNameIsNull_returnsFalse() throws IllegalValueException {
+        Name name1 = new Name("John");
 
         assertEquals(false, name1.isSimilar(null));
+    }
 
-        name2 = new Name("john");
+    @Test
+    public void isSimilar_1exactNameDifferentCase_returnsTrue() throws IllegalValueException {
+        Name name1 = new Name("John");
+        Name name2 = new Name("john");
 
         assertEquals(true, name1.isSimilar(name2));
+    }
 
-        name2 = new Name("Mary");
+    @Test
+    public void isSimilar_2exactNameDifferentCase_returnsTrue() throws IllegalValueException {
+        Name name1 = new Name("John sMith");
+        Name name2 = new Name("john Smith");
+
+        assertEquals(true, name1.isSimilar(name2));
+    }
+
+    @Test
+    public void isSimilar_differentName_returnsFalse() throws IllegalValueException {
+        Name name1 = new Name("John sMith");
+        Name name2 = new Name("Mary");
 
         assertEquals(false, name1.isSimilar(name2));
+    }
 
-        name2 = new Name("JoHn");
-
-        assertEquals(true, name1.isSimilar(name2));
-
-        name1 = new Name("John Smith");
-        name2 = new Name("Smith John");
-
-        assertEquals(true, name1.isSimilar(name2));
-
-        name2 = new Name("jOhn SmiTh");
+    @Test
+    public void isSimilar_name1SubsetOfName2_returnsTrue() throws IllegalValueException {
+        Name name1 = new Name("John");
+        Name name2 = new Name("John sMith");
 
         assertEquals(true, name1.isSimilar(name2));
+    }
 
-        name2 = new Name("John");
+    @Test
+    public void isSimilar_name2SubsetOfName1_returnsTrue() throws IllegalValueException {
+        Name name1 = new Name("John sMith");
+        Name name2 = new Name("John");
 
         assertEquals(true, name1.isSimilar(name2));
+    }
 
-        name1 = new Name("John");
-        name2 = new Name("John sMith");
-
-        assertEquals(true, name1.isSimilar(name2));
-        
-        name1 = new Name("John Jacob");
-        name2 = new Name("John sMith");
+    @Test
+    public void isSimilar_sameFirstNameDifferentLastName_returnsFalse()
+            throws IllegalValueException {
+        Name name1 = new Name("John Jacob");
+        Name name2 = new Name("John sMith");
 
         assertEquals(false, name1.isSimilar(name2));
-        
-        name1 = new Name("John K Smith");
-        name2 = new Name("John Smith");
+    }
+
+    @Test
+    public void isSimilar_name1SupersetOfName2_returnsTrue() throws IllegalValueException {
+        Name name1 = new Name("John Smith");
+        Name name2 = new Name("John K Smith");
 
         assertEquals(true, name1.isSimilar(name2));
-        
-        name1 = new Name("John Smith");
-        name2 = new Name("John K Smith");
+    }
+
+    @Test
+    public void isSimilar_name2SupersetOfName1_returnsTrue() throws IllegalValueException {
+        Name name1 = new Name("John K Smith");
+        Name name2 = new Name("John Smith");
 
         assertEquals(true, name1.isSimilar(name2));
     }
