e8fbc7123499982a05da654c96a654271290ebf9
Wei Kang <weikangchia@gmail.com>
2016-09-04 17:31:59 +0800

    Modify isSimilar method Now the test case will return false if the other name object is a null

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -63,9 +63,12 @@ public class Name {
 
     /**
      * Returns true of the other name is very similar to this name.
-     * Two names are considered similar if ...
+     * Two names are considered similar if they are equal
      */
     public Object isSimilar(Name other) {
+        if(other == null) {
+            return false;
+        }
         return true;
     }
 
